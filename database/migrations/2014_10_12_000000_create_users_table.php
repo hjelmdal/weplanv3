469925e3c1957e39386c3c145b88b4c25b264bf6
Stephan Nielsen - os x <stephan@pixel8.dk>
2019-02-15 15:47:30 +0100

    Added Laravel application with User Login

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+    
+            $table->string('api_token',100)->nullable();
+            $table->string('avatar')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        });
+    
+        Schema::create('users_facebook', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('token');
+            $table->string('refresh_token')->nullable();
+            $table->integer('expires_in');
+            $table->string('facebook_id');
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    
+        Schema::create('users_google', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('token');
+            $table->string('refresh_token')->nullable();
+            $table->integer('expires_in');
+            $table->string('google_id');
+            $table->index('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_google');
+        Schema::dropIfExists('users_facebook');
+    
+    
+    }
+}
