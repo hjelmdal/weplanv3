63072133f096a71aec6e3ca4136b0f001b72d7bd
Stephan Nielsen - os x <stephan@pixel8.dk>
2019-02-16 02:12:10 +0100

    Corrected spinner

--- resources/views/layouts/theme/partials/_layout-page-loader.blade.php
+++ resources/views/layouts/theme/partials/_layout-page-loader.blade.php
@@ -1,6 +1,6 @@
 <!-- begin::Page loader -->
 <div class="k-page-loader k-page-loader--logo">
-    <img alt="Logo" src="{{ config("app.keen_assets") }}/media/logos/logo-launcher-2.png"/>
-    <div class="k-spinner k-spinner--danger"></div>
+    <img alt="Logo" src="/img/weplan_324x90_black.png" height="45"/>
+    <div class="k-spinner k-spinner--primary"></div>
 </div>
 <!-- end::Page Loader -->
