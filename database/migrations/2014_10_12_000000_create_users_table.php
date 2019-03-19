ec95c1badcb7b9c19e259c4a538c80feeb69113d
Stephan <shn@impact.dk>
2019-03-19 16:22:29 +0100

    Updated user model - belongs to player

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('player_id',10)->nullable();
             $table->rememberToken();
     
             $table->string('api_token',100)->nullable();
