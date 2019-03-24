55d4f63f3f9d6c07f93c8d02e3f530708da6c4d7
Stephan <shn@impact.dk>
2019-03-24 23:33:08 +0100

    Git version in footer

--- resources/views/layouts/theme/partials/_footer-base.blade.php
+++ resources/views/layouts/theme/partials/_footer-base.blade.php
@@ -1,6 +1,6 @@
 <!-- begin:: Footer -->
 <div class="kt-footer kt-grid__item" id="kt_footer">
-    <div class="kt-footer__copyright">2019&nbsp;&copy;&nbsp;<a href="https://hjelmdal.dk" target="_blank" class="kt-link">Pixel8</a> @if($user && $user->hasRole("admin")) {{  }} @endif</div>
+    <div class="kt-footer__copyright">2019&nbsp;&copy;&nbsp;<a href="https://hjelmdal.dk" target="_blank" class="kt-link">Pixel8</a>&nbsp;&nbsp; @if($user && $user->hasRole("super-admin")) {{ "Version: " . Helpers::gitVersion()->getShortVersion() }} @endif</div>
     <div class="kt-footer__menu"><a href="/" target="_blank" class="kt-link">About</a><a href="/" target="_blank" class="kt-link">Team</a><a href="/" target="_blank" class="kt-link">Contact</a></div>
 </div>
 <!-- end:: Footer -->
