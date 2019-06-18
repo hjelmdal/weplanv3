54fc142a30a4d7f7a78438f9a421e9759e5f5d25
Stephan <shn@impact.dk>
2019-06-19 00:44:01 +0200

    Padding issue

--- resources/views/app/activities/index.blade.php
+++ resources/views/app/activities/index.blade.php
@@ -62,7 +62,7 @@
                 <div class="kt-portlet__body">
                     <div class="kt-section__content kt-section__content--border">
                         <div class="row">
-                            <div class="col-12">
+                            <div class="col-12 kt-padding-0 text-center">
                                 <template v-for="type in types">
                                     <label class="kt-checkbox kt-checkbox--solid kt-padding-r-5" style="padding-left: 19px;" v-bind:class="{'kt-checkbox--success':(type.id == 1), 'kt-checkbox--warning':(type.id == 5), 'kt-checkbox--danger':(type.id == 2), 'kt-checkbox--primary':(type.id == 3), 'kt-checkbox--warning':(type.id == 4)}">
                                         <input v-model="filters[type.id]" type="checkbox" @change="activitiesLoad('reload')"> @{{ type.name }}
