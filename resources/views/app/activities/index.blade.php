dc28a70b8bf7700665dbc9406617309cb840b70d
Stephan <shn@impact.dk>
2019-06-18 02:07:40 +0200

    Today button via Axios

--- resources/views/app/activities/index.blade.php
+++ resources/views/app/activities/index.blade.php
@@ -64,7 +64,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <template v-for="type in types">
-                                    <label class="kt-checkbox kt-checkbox--solid kt-padding-r-5 kt-padding-l-20" v-bind:class="{'kt-checkbox--success':(type.id == 1), 'kt-checkbox--warning':(type.id == 5), 'kt-checkbox--danger':(type.id == 2), 'kt-checkbox--primary':(type.id == 3), 'kt-checkbox--warning':(type.id == 4)}">
+                                    <label class="kt-checkbox kt-checkbox--solid kt-padding-r-5" style="padding-left: 19px;" v-bind:class="{'kt-checkbox--success':(type.id == 1), 'kt-checkbox--warning':(type.id == 5), 'kt-checkbox--danger':(type.id == 2), 'kt-checkbox--primary':(type.id == 3), 'kt-checkbox--warning':(type.id == 4)}">
                                         <input v-model="filters[type.id]" type="checkbox" @change="activitiesLoad('reload')"> @{{ type.name }}
                                         <span></span>
                                     </label>
