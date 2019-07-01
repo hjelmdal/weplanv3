a8920d739acd11aaf3c7363075b41ff54c54ae85
Morten Spjuth <mortenspjuth@Mortens-MBP.lan>
2019-07-01 20:04:54 +0200

    updated with fk on we_players table

--- database/migrations/2019_06_28_054541_update_fk_div_tables.php
+++ database/migrations/2019_06_28_054541_update_fk_div_tables.php
@@ -21,6 +21,17 @@ class UpdateFkDivTables extends Migration
             $table->foreign('player_id')->references('id')->on('we_players');
             $table->foreign('category_id')->references('id')->on('we_decline_categories');
             $table->foreign('type_id')->references('id')->on('we_activity_types');
+            
+
+        });
+
+        Schema::table('we_players', function (Blueprint $table) {            
+            $table->integer("team_id")->unsigned()->change();
+            $table->integer("user_id")->unsigned()->change();
+
+            $table->foreign('team_id')->references('id')->on('we_teams');
+            $table->foreign('user_id')->references('id')->on('users');
+            // $table->foreign('dbf_id')->references('dbf_id')->on('bp_players');
 
         });
     }
