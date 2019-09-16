9243aeaa865a6aeb459cdc82075310f96c70ac66
Stephan Nielsen - os x <stephan@pixel8.dk>
2019-09-16 19:05:34 +0200

    update

---
+++ resources/views/layouts/metronic/_layout.blade.php
@@ -0,0 +1,44 @@
+<!-- begin:: Page -->
+
+@include("layouts.metronic.partials._header.base-mobile")
+<div class="kt-grid kt-grid--hor kt-grid--root">
+    <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--ver kt-page">
+
+        @include("layouts.metronic.partials._aside.base")
+        <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
+
+            @include("layouts.metronic.partials._header.base")
+            <div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
+
+                @include("layouts.metronic.partials._subheader.subheader-v3")
+
+            <!-- Content area -->
+                    <div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+                        <!--Begin::Dashboard 1-->
+                        <!--Begin::Row-->
+                        <div class="row">
+                            <div class="col-12">
+                                @yield("content")
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+            @include("layouts.metronic.partials._footer.base")
+        </div>
+    </div>
+</div>
+
+<!-- end:: Page -->
+
+@include("layouts.metronic.partials._search-panel")
+
+<!--[html-partial:include:{"file":"partials/_scrolltop.html"}]/-->
+
+<!--[html-partial:include:{"file":"partials/_toolbar.html"}]/-->
+
+<!--[html-partial:include:{"file":"partials/_demo-panel.html"}]/-->
+
+<!--[html-partial:include:{"file":"partials/_chat.html"}]/-->
+
+@include("layouts.metronic.partials._profile-panel")
---
+++ resources/views/layouts/metronic/partials/_header/base.blade.php
@@ -0,0 +1,10 @@
+
+<!-- begin:: Header -->
+<div id="kt_header" class="kt-header kt-grid__item  kt-header--fixed ">
+
+	@include("layouts.metronic.partials._header._menu")
+
+	@include("layouts.metronic.partials._header.topbar._base")
+</div>
+
+<!-- end:: Header -->
