c80b7b3450fa7d567ef689605fcc734e750a01a2
Stephan Hjelmdal <stephan@pixel8.dk>
2019-10-17 00:10:56 +0200

    Design fixes

--- resources/assets/vuejs/teams/components/TeamForm.vue
+++ resources/assets/vuejs/teams/components/TeamForm.vue
@@ -89,7 +89,7 @@ export default {
             <label class="col-form-label col-lg-3 col-sm-12">Max antal spillere</label>
 
             <div class="col-lg-6 col-md-9 col-sm-12">
-                <input name="max_players" :class="errors.max_players ? 'is-invalid': ''" class="form-control kt-input" id="max_players" type="number" placeholder="20" v-model="form.max_players">
+                <input name="max_players" :class="errors.max_players ? 'is-invalid': ''" class="form-control kt-input" id="max_players" type="tel" placeholder="20" v-model="form.max_players">
                 <div v-for="error in errors.max_players" class="invalid-feedback">{{ error.replace("max players","Feltet") }}</div>
                 <span class="kt-form__help">Hvor mange ønsker du max på denne trup?</span>
 
